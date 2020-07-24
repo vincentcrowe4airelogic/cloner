c9d9b6cba7234fd7aa2a6523da700e758757b6ab
Vincent Crowe <vincent.crowe@airelogic.com>
2020-07-24 10:47:34 +0100

    implement stream upload

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,5 +1,6 @@
 import { Cred, Clone, CloneOptions } from "nodegit";
 import fs from "fs";
+import stream from "stream";
 import archiver from "archiver";
 import AWS from "aws-sdk";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
@@ -12,7 +13,7 @@ const options : CloneOptions = {
         callbacks: {
             certificateCheck: function() { return 0; },
             credentials: function() {
-                return Cred.sshKeyNew('user', './key.pub', './key.prk', '');
+                return Cred.sshKeyNew('user', './tmp/ssh/key.pub', './tmp/ssh/key.prk', '');
             }
         }
     }
@@ -23,29 +24,29 @@ export const repoToBucket = async (
   ): Promise<APIGatewayProxyResult> => {
     const repo = process.env.REPO!;
     const bucketName = process.env.BUCKET!;
-    fs.writeFileSync("./key.prk", await getParameter(`/ssh/${repo}/prk`));
-    fs.writeFileSync("./key.pub", await getParameter(`/ssh/${repo}/pub`));
-    await Clone.clone(`ssh://user@bitbucket.org/${repo}.git`, "repo", options);
-    zipDirectory("repo", "repo.zip");
-    await s3.upload({
-        Bucket: bucketName,
-        Key: "repo.zip"
-    }).promise();
+    fs.writeFileSync("./tmp/ssh/key.prk", await getParameter(`/ssh/${repo}/prk`));
+    fs.writeFileSync("./tmp/ssh/key.pub", await getParameter(`/ssh/${repo}/pub`));
+    await Clone.clone(`ssh://user@bitbucket.org/${repo}.git`, "./tmp/repo", options);
+    zipDirectory("./tmp/repo", "./tmp/repo.zip");
+    const zipStream = fs.createReadStream("./tmp.repo.zip");
 
+    const { s3Stream, awaiter} = uploadFromStream(bucketName, "repo.zip");
+    zipStream.pipe(s3Stream);
+
+    await awaiter;
+    
     return {
       statusCode: 200,
       body: ""
     };
   }
 
-export const test = async (
-  event: APIGatewayProxyEvent
-  ): Promise<APIGatewayProxyResult> => {
-    console.log(JSON.stringify(event))
-    return {
-      statusCode: 200,
-      body: ""
-    }
+const uploadFromStream = (bucketName: string, key: string) => {
+  const s3Stream = new stream.PassThrough();
+  const params = {Bucket: bucketName, Key: key, Body: s3Stream};
+  const awaiter = s3.upload(params).promise();
+
+  return {s3Stream, awaiter};
 }
 
 const getParameter = (key: string) : Promise<string> => {
